a3369d1f12d631f2dcc184cfa24b33d5949b7984
yykuso <uecmr.y@gmail.com>
2024-12-07 23:53:03 +0900

    MapLibre版でお試し

---
+++ js/common.js
@@ -0,0 +1,304 @@
+import OpacityControl from './maplibre-gl-opacity.js';
+
+
+// ########################################
+//  URL Query Parameter
+// ########################################
+// URLから緯度・経度・ズームを取得
+function getMapStateFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    const lat = parseFloat(params.get("lat")) || 35.3622;
+    const lng = parseFloat(params.get("lng")) || 138.7313;
+    const zoom = parseFloat(params.get("zoom")) || 4;
+    return { lat, lng, zoom };
+}
+
+// URLに緯度・経度・ズームを反映
+function updateURL(lat, lng, zoom) {
+    const params = new URLSearchParams();
+    params.set("lat", lat.toFixed(5));
+    params.set("lng", lng.toFixed(5));
+    params.set("zoom", zoom.toFixed(2));
+    history.replaceState(null, "", `?${params.toString()}`);
+}
+
+// URLから初期状態を取得
+const { lat, lng, zoom } = getMapStateFromURL();
+
+
+// ########################################
+//  Map
+// ########################################
+const map = new maplibregl.Map({
+    container: 'map',
+    center: [lng, lat],
+    zoom: zoom,
+    // style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json'
+    style: {
+        version: 8,
+        sources: {},
+        layers: [],
+        glyphs: 'https://glyphs.geolonia.com/{fontstack}/{range}.pbf'
+    },
+});
+
+// Map本体
+map.on('load', () => {
+    // ####################
+    //  Tiles
+    const osmAttribution = "© <a href='https://www.openstreetmap.org/copyright/' target='_blank'>OpenStreetMap</a> contributors";
+    const gsiAttribution = "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>国土地理院</a>";
+
+    // OpenStreetMap
+    map.addSource('osm_tiles', {
+        type: 'raster',
+        tiles: [
+            'https://a.tile.openstreetmap.jp/{z}/{x}/{y}.png',
+            'https://b.tile.openstreetmap.jp/{z}/{x}/{y}.png',
+        ],
+        tileSize: 256,
+        attribution: osmAttribution,
+    });
+    map.addLayer({
+        id: 'osm_tiles',
+        type: 'raster',
+        source: 'osm_tiles',
+        minzoom: 2,
+        maxzoom: 18,
+    });
+
+    // OpenTopoMap
+    map.addSource('otm_tiles', {
+        type: 'raster',
+        tiles: [
+            'https://a.tile.opentopomap.org/{z}/{x}/{y}.png',
+            'https://b.tile.opentopomap.org/{z}/{x}/{y}.png'
+        ],
+        tileSize: 256,
+        attribution: osmAttribution,
+    });
+    map.addLayer({
+        id: 'otm_tiles',
+        type: 'raster',
+        source: 'otm_tiles',
+        minzoom: 2,
+        maxzoom: 18,
+    });
+
+    // GSI Pale
+    map.addSource('gsi_pale_tiles', {
+        type: 'raster',
+        tiles: ['https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png'],
+        tileSize: 256,
+        attribution: gsiAttribution,
+    });
+    map.addLayer({
+        id: 'gsi_pale_tiles',
+        type: 'raster',
+        source: 'gsi_pale_tiles',
+        minzoom: 2,
+        maxzoom: 18,
+    });
+
+    // GSI Std
+    map.addSource('gsi_std_tiles', {
+        type: 'raster',
+        tiles: ['https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png'],
+        tileSize: 256,
+        attribution: gsiAttribution,
+    });
+    map.addLayer({
+        id: 'gsi_std_tiles',
+        type: 'raster',
+        source: 'gsi_std_tiles',
+        minzoom: 2,
+        maxzoom: 18,
+    });
+
+    // GSI Blank
+    map.addSource('gsi_blank_tiles', {
+        type: 'raster',
+        tiles: ['https://cyberjapandata.gsi.go.jp/xyz/blank/{z}/{x}/{y}.png'],
+        tileSize: 256,
+        attribution: gsiAttribution,
+    });
+    map.addLayer({
+        id: 'gsi_blank_tiles',
+        type: 'raster',
+        source: 'gsi_blank_tiles',
+        minzoom: 5,
+        maxzoom: 18,
+    });
+
+    // ####################
+    //  Layers
+    // GSI Seamlessphoto
+    map.addSource('gsi_photo_tiles', {
+        type: 'raster',
+        tiles: ['https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg'],
+        tileSize: 256,
+        attribution: gsiAttribution,
+    });
+    map.addLayer({
+        id: 'gsi_photo_tiles',
+        type: 'raster',
+        source: 'gsi_photo_tiles',
+        minzoom: 2,
+        maxzoom: 18,
+        paint: {
+            'raster-opacity': 0.5,
+        }
+    });
+
+    // OpenSeaMap
+    map.addSource('openseamap_tiles', {
+        type: 'raster',
+        tiles: ['https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png'],
+        tileSize: 256,
+        attribution: "Map style: <a href='https://www.openseamap.org' target='_blank'>OpenSeaMap</a> contributors",
+    });
+    map.addLayer({
+        id: 'openseamap_tiles',
+        type: 'raster',
+        source: 'openseamap_tiles',
+        minzoom: 2,
+        maxzoom: 18,
+    });
+
+    // OpenRailwayMap
+    map.addSource('openrailwaymap_tiles', {
+        type: 'raster',
+        tiles: [
+            'https://a.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png',
+            'https://b.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png',
+        ],
+        tileSize: 256,
+        attribution: "Map style: &copy; <a href='https://www.OpenRailwayMap.org' target='_blank'>OpenRailwayMap</a>",
+    });
+    map.addLayer({
+        id: 'openrailwaymap_tiles',
+        type: 'raster',
+        source: 'openrailwaymap_tiles',
+        minzoom: 2,
+        maxzoom: 19,
+    });
+
+    // 航路情報
+    map.addSource('sea_route_layers', {
+        type: 'geojson',
+        data: './data/seaRouteData.geojson',
+    });
+    map.addLayer({
+        id: 'sea_route_layers_outline',
+        type: 'line',
+        source: 'sea_route_layers',
+        layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        paint: {
+            'line-color': '#FFFFFF',
+            'line-width': 9,
+            'line-opacity': 0.5
+        }
+    });
+    map.addLayer({
+        id: 'sea_route_layers_line',
+        type: 'line',
+        source: 'sea_route_layers',
+        layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        paint: {
+            'line-color': ['coalesce', ['get', 'color'], '#000000'],
+            'line-width': 3,
+        }
+    });
+    map.addLayer({
+        id: 'sea_route_layers_name',
+        type: 'symbol',
+        source: 'sea_route_layers',
+        layout: {
+            'symbol-placement': 'line',
+            "text-offset": [0, 1],
+            'text-field': [
+                'format', 
+                ['get', 'businessName'],{},
+                ' (',{},
+                ['get', 'routeName'],{},
+                ') ',{}
+            ],
+            'text-font': ['Noto Sans CJK JP Regular'],
+            'text-size': 9
+        },
+        paint: {
+            'text-color': ['coalesce', ['get', 'color'], '#000000'],
+            'text-halo-color': '#FFFFFF',
+            'text-halo-width': 2,
+            'text-halo-blur': 2
+        }
+    });
+
+    // BaseLayer
+    const mapBaseLayer = {
+        osm_tiles: 'OpenStreetMap',
+        otm_tiles: 'OpenTopoMap',
+        gsi_pale_tiles: '地理院 淡色地図',
+        gsi_std_tiles: '地理院 標準地図',
+        gsi_blank_tiles: '地理院 白地図',
+    };
+    // OverLayer
+    const mapOverLayer = {
+        gsi_photo_tiles: '地理院 オルソ',
+        openseamap_tiles: 'OpenSeaMap',
+        openrailwaymap_tiles: 'openRailwayMap',
+    };
+    // OpacityControl
+    let Opacity = new OpacityControl({
+        baseLayers: mapBaseLayer,
+        overLayers: mapOverLayer,
+        opacityControl: false,
+    });
+    map.addControl(Opacity, 'top-right');
+});
+
+// Navigation Controls
+map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
+map.addControl(new maplibregl.GeolocateControl(), 'bottom-right');
+map.addControl(new maplibregl.ScaleControl(), 'bottom-left');
+
+// ポップアップを無効化
+const popup = new maplibregl.Popup({
+    closeButton: true,
+    closeOnClick: true
+});
+
+// ラインをクリックしたときのイベントを登録
+map.on('click', 'sea_route_layers_line', (event) => {
+    // クリックしたラインのプロパティを取得
+    const properties = event.features[0].properties;
+
+    // ポップアップ内容
+    const popupContent = `
+        <div>
+            ${properties.businessName}<br>
+            ${properties.routeName}<br>
+            （${properties.portName1}～${properties.portName2}）<br>
+            <a href="${properties.url}" target="_blank">運航スケジュール</a><br>
+        </div>
+    `;
+
+    // ポップアップをクリック地点に表示
+    popup
+        .setLngLat(event.lngLat)
+        .setHTML(popupContent)
+        .addTo(map);
+});
+
+// マップ移動時にURLを更新
+map.on("moveend", () => {
+    const center = map.getCenter();
+    const zoom = map.getZoom();
+    updateURL(center.lat, center.lng, zoom);
+});
