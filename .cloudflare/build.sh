9eba709457bfb75fe72f4a726a003018656859e0
yykuso <uecmr.y@gmail.com>
2026-04-19 12:07:22 +0900

    build.shのファイルコピー処理をrsyncからcpに変更し、不要なファイルの削除処理を追加

--- .cloudflare/build.sh
+++ .cloudflare/build.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 set -e
 
+# スクリプトの場所に関わらずリポジトリのルートに移動
+cd "$(dirname "$0")/.."
+
 # 1. 成果物用ディレクトリの作成
+# 既存のdeployがあれば一旦消してクリーンにする
+rm -rf deploy
 mkdir -p deploy
 
-# 2. ファイルのコピー
-rsync -av --exclude='deploy/' --exclude='.github/' --exclude='.git/' --exclude='.gitignore' --exclude='README.md' --exclude='_config.yml' ./ ./deploy/
+# 2. ファイルのコピー (rsyncの代わりに cp を使用)
+# 必要なものだけをコピーする方式に変更します
+cp -r * deploy/
 
-# 3. GeoJSONの座標丸めと最適化
+# 3. 不要なファイルを deploy/ から削除 (rsyncの--excludeと同じ役割)
+cd deploy
+rm -rf deploy .github .git .cloudflare .gitignore README.md _config.yml
+cd ..
+
+# 4. GeoJSONの座標丸めと最適化
 find deploy -name '*.geojson' | while read file; do
   jq 'def r: if type=="array" then map(r) elif type=="object" then with_entries(.value |= r) elif type=="number" then (.*100000|round)/100000 else . end;
       . as $in | if has("features") then .features |= map(if has("geometry") and .geometry.coordinates then .geometry.coordinates |= (r) else . end)
       elif has("geometry") and .geometry.coordinates then .geometry.coordinates |= (r) else . end' "$file" | jq -c . > tmp.geojson && mv tmp.geojson "$file"
 done
 
-# 4. JSONの最適化
+# 5. JSONの最適化
 find deploy -name '*.json' ! -name '*.geojson' | while read file; do
   jq -c . "$file" > tmp.json && mv tmp.json "$file"
 done
 
-# 5. JS/CSSの最適化 (npxを使用してインストール不要に)
+# 6. JS/CSSの最適化 (npxを使用してインストール不要に)
 find deploy -name '*.js' | while read file; do
   npx terser "$file" -c -m -o "$file.min" && mv "$file.min" "$file"
 done
 
 find deploy -name '*.css' | while read file; do
-  npx esbuild "$file" --minify --outfile="$file.min" && mv "$file.min" "$file"
+  npx -p esbuild esbuild "$file" --minify --outfile="$file.min" && mv "$file.min" "$file"
 done
 
-# 6. サイトマップとrobots.txtの生成
+# 7. サイトマップとrobots.txtの生成
 BASE_URL="https://searoute.info"
 echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' > deploy/sitemap.xml
 find deploy -name "*.html" | while read filepath; do
