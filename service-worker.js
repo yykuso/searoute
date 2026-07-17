63b73271752e4f63f5f5c75565f3317d97ac3a11
yykuso <uecmr.y@gmail.com>
2026-07-17 20:53:26 +0900

    サービスワーカーのキャッシュ管理を改善し、バージョンを更新

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'searoute-v1.2';
+const CACHE_NAME = 'searoute-v1.3';
 const urlsToCache = [
   './',
   './img/favicon.ico',
@@ -8,23 +8,67 @@ const urlsToCache = [
   './style/empty.json'
 ];
 
+function clearAllCaches() {
+  return caches.keys().then((cacheNames) => {
+    return Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+  });
+}
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
         return cache.addAll(urlsToCache);
       })
+      .then(() => self.skipWaiting())
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName))
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (!event.data || !event.data.type) {
+    return;
+  }
+
+  if (event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+    return;
+  }
+
+  if (event.data.type === 'PURGE_ALL_CACHES') {
+    event.waitUntil(clearAllCaches());
+  }
+});
+
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then((response) => {
-        if (response) {
-          return response;
+        if (response && response.ok && new URL(event.request.url).origin === self.location.origin) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
         }
-        return fetch(event.request);
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
       })
   );
 });
